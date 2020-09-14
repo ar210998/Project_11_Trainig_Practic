26a6d0ae1941f5c242b34caa09391d45fa511c49
Aleksandr. R <ar210998@europe.com>
2020-09-14 08:46:47 +0300

    carousel slick v2

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
 		$('.carousel__inner').slick({
+			lazyLoad: 'progressive',
+			//fade: true,
 			speed: 1000,
+			//vertical: true,
+			//rtl: true,
 			//centerMode: true,
-			//draggable: false,
+			draggable: false,
 			autoplay: true,
 			autoplaySpeed: 1000,
 			//adaptiveHeight: true,
-			prevArrow: '<button type="button" class="slick-button slick-button_prev"><img src="../ico/4.Carousel/left.png"></button>',
-			nextArrow: '<button type="button" class="slick-button slick-button_next"><img src="../ico/4.Carousel/right.png"></button>',
+			prevArrow: '<button type="button" class="slick-button slick-button_prev"><img src="ico/4.Carousel/left.png"></button>',
+			nextArrow: '<button type="button" class="slick-button slick-button_next"><img src="ico/4.Carousel/right.png"></button>',
 			responsive: [
 				{
 				  breakpoint: 768,
