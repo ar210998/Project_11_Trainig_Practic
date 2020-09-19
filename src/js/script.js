9d268b1294851c88219a717d60d992fc6b0e2cff
Aleksandr. R <ar210998@europe.com>
2020-09-19 10:36:25 +0300

    jQuery 3.51

--- src/js/script.js
+++ src/js/script.js
@@ -45,9 +45,91 @@ $(document).ready(function(){
 					e.preventDefault();
 					$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
 					$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-				})
+				});
 			  });
-		  };
+		  }
 		  toggleSlide('.catalog-item__link');
 		  toggleSlide('.catalog-item__back');
+
+	/* модальные окна */		  
+
+	$('[data-modal=consultation]').on('click', function(){
+		$('.overlay, #consultation').fadeIn(100);
 	});
+	$('.button_buy').each(function(i){
+		$(this).on('click', function(){
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn(100);
+		});
+	});
+	$('.modal__close').on('click', function(){
+		$('.overlay, #consultation, #order, #thanks').fadeOut(1000);
+	});
+
+	function validateForms(form) {
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					  minlength: 2
+				},
+				phone: {
+					required: true
+				},
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: {
+					required: "Имя скажи!",
+					minlength: jQuery.validator.format("Не менее {0} буковок введи!")
+				  },
+				phone: {
+					required: "Гони телефон!",
+				  },
+				email: {
+				  required: "Почту давай!",
+				  email: "Не пизди!"
+				}
+			  }
+		});
+	}
+	validateForms('#consultation-form');
+	validateForms('#consultation form');
+	validateForms('#order form');
+
+$('input[name=phone]').mask('+372 99999999');
+
+$('form').submit(function(e) {
+	e.preventDefault();
+	$.ajax({
+		type: "POST",
+		url: "mailer/smart.php",
+		data: $(this).serialize()
+	}).done(function() {
+		$(this).find('input').val('');
+		$('#consultation, #order').fadeOut(100);
+		$('overlay, #thanks').fadeIn(1000);
+		$('form').trigger('reset');
+	});
+	return false;
+});
+
+
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 700) {
+			$('.to-top').fadeIn();
+		} else {
+			$('.to-top').fadeOut();
+		}
+	});
+	
+	$("a[href^='#']").click(function(){
+		var _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+});
+
+});
